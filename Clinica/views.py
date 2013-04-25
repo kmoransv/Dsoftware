a8665714facd854ac7a71bce1f1a02d1e1b13088
Ever Jimenez Dell <ever.jimenez@catolica.edu.sv>
2013-04-25 17:46:49 -0600

    Control de Acceso desarrollado, incluyendo inicio y cerrar seción.

--- Clinica/views.py
+++ Clinica/views.py
@@ -18,3 +18,25 @@ import datetime
 
 def index(request):
     return render_to_response("Clinica.html", context_instance=RequestContext(request))
+def Acceso(request):
+    if request.method == "POST":
+        iFrmAcceso = AuthenticationForm(request.POST)
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect("/index/Administracion/")
+            else:
+                return HttpResponse("Verifica si tu Usuario esta Activo")
+        else:
+            return HttpResponse("Usuario no Existe")
+
+    else:
+        iFrmAcceso = AuthenticationForm()
+    return render_to_response("AccesoWeb.html", {'iFrmAcceso':iFrmAcceso}, context_instance=RequestContext(request))
+@login_required(login_url='/Acceso/')
+def Cerrar(request):
+    logout(request)
+    return HttpResponseRedirect("/")
