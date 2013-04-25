6954d3e7c996da40fbc37e4d2aac947ca0855a33
Francisco Padilla <francisco.padilla@catolica.edu.sv>
2013-04-25 17:58:49 -0600

    Registro de paciente,consulta de paciente y eliminacion de paciente finalizado con sus respectivas plantillas.

--- Clinica/views.py
+++ Clinica/views.py
@@ -44,3 +44,50 @@ def Cerrar(request):
 @login_required(login_url='/Acceso/')
 def indexP(request):
     return render_to_response("ClinicaP.html", context_instance=RequestContext(request))
+
+@permission_required('auth.Can add permission', login_url='/Acceso/')
+def Registros(request):
+    return HttpResponse("Registros")
+
+def AgregarPaciente(request):
+    if request.method == 'POST':
+        iFrmPaciente = FrmPaciente(request.POST)
+        if iFrmPaciente.is_valid():
+            try:
+                U = User.objects.create_user(request.POST['dui_paciente'],
+                                             'notiene@notiene.com',
+                                             request.POST['apellido_paciente'])
+                U.save()
+                idU = User.objects.latest('id')
+                '''idG = Group.objects.get(pk=3)
+                idG.user_set.add(idU)'''
+
+                uPaciente = User.objects.latest('id')
+                permiso = Permission.objects.get(name='Can add tbl paciente')
+                uPaciente.user_permissions.add(permiso)
+
+                NiFrmPaciente = iFrmPaciente.save(commit=False)
+                NiFrmPaciente.user = idU
+                NiFrmPaciente.save()
+                return HttpResponseRedirect("/Acceso/")
+            except Exception as e:
+                return HttpResponse("usuario ya existe")
+
+    else:
+        iFrmPaciente = FrmPaciente()
+    return render_to_response("AgregarPaciente.html",
+                              {"iFrmPaciente":iFrmPaciente},
+                              context_instance=RequestContext(request))
+@permission_required('auth.Can add permission', login_url='/Acceso/')
+def ConsultarPaciente(request):
+    iTblPaciente = TblPaciente.objects.all()
+    return render_to_response("ConsultarPaciente.html",
+                              {"iTblPaciente":iTblPaciente},
+                              context_instance=RequestContext(request))
+@permission_required('auth.Can add permission', login_url='/Acceso/')
+def EliminarPaciente(request, id_paciente):
+    paciente = TblPaciente.objects.get(pk=id_paciente)
+    usuario = User.objects.get(username = id.dui_paciente)
+    paciente.delete()
+    usuario.delete()
+    return HttpResponseRedirect("/Usuarios/Consultar/Pacientes/")
